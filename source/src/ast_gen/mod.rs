124ede9483c71440ed381659e80d61bef87bd810
lions-language <lions_language@163.com>
2021-04-20 22:48:41 +0800

    update ast_gen

--- source/src/ast_gen/mod.rs
+++ source/src/ast_gen/mod.rs
@@ -24,7 +24,16 @@ impl Generator {
 
     fn gen_const(self, node: Box<ConstAstNode>) {
         /*
-         *
+         * 生成指令
+         * 1. 将常量放入到 栈中
+         * */
+    }
+
+    fn gen_binary_operator(self, node: Box<BinaryOperator>) {
+        /*
+         * 生成指令
+         * 1. 判断左边和右边的类型
+         * 2. 根据类型, 在字节码中指定读取多少字节
          * */
     }
 
