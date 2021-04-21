011b240f0dcfa3dddfb22ee0e4c54a4dd0adb696
lions-language <lions_language@163.com>
2021-04-21 22:50:19 +0800

    add anno

--- source/src/ast_gen/mod.rs
+++ source/src/ast_gen/mod.rs
@@ -1,13 +1,19 @@
 use crate::ast::{AstNode};
 
+enum Type {
+}
+
+struct NodeData {
+    typ Type
+}
+
 struct Generator {
-    root: AstNode
 }
 
 impl Generator {
-    pub fn generate(self) {
+    pub fn generate(self, node: AstNode) {
         use AstNod::*;
-        match self.root {
+        match node {
             SingleOperator(_) => {
             },
             BinaryOperator(_) => {
@@ -22,22 +28,24 @@ impl Generator {
         }
     }
 
-    fn gen_const(self, node: Box<ConstAstNode>) {
+    fn gen_const(self, node: Box<ConstAstNode>) -> NodeData {
         /*
          * 生成指令
          * 1. 将常量放入到 栈中
          * */
     }
 
-    fn gen_binary_operator(self, node: Box<BinaryOperator>) {
+    fn gen_binary_operator(self, node: Box<BinaryOperator>) -> NodeData {
         /*
          * 生成指令
          * 1. 判断左边和右边的类型
-         * 2. 根据类型, 在字节码中指定读取多少字节
+         * 2. 生成函数调用的指令
+         *  - 从栈中读取几个参数
+         *  - 返回值存放在哪里
          * */
     }
 
-    pub fn new(node: AstNode) -> Self {
+    pub fn new() -> Self {
         Self {
         }
     }
