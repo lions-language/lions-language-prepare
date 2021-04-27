c3306d9ed6bb5f34c6b4bd7047b6de63f3e6320e
lions-language <lions_language@163.com>
2021-04-27 23:29:19 +0800

    fix compile error

--- source/src/ast_gen/mod.rs
+++ source/src/ast_gen/mod.rs
@@ -1,4 +1,6 @@
-use crate::ast::{AstNode};
+use crate::ast::{AstNode, ConstAstNode, SingleOperatorAstNode
+    , BinaryOperatorAstNode};
+use crate::token::{TokenType};
 
 enum PackageStr {
     Inner,
@@ -6,9 +8,14 @@ enum PackageStr {
     Other(String)
 }
 
+enum ModuleStr {
+    Inner,
+    Named(String)
+}
+
 struct Type {
     package_str: PackageStr,
-    moduler_str: String,
+    moduler_str: ModuleStr,
     typ_str: String
 }
 
@@ -42,24 +49,28 @@ impl Generator {
          * 生成指令
          * 1. 将常量放入到 栈中
          * */
-        match node.context().typ {
+        let context = node.token.context();
+        match context.typ {
             TokenType::U32(value) => {
                 return NodeData{
                     typ: Type{
                         package_str: PackageStr::Inner,
+                        moduler_str: ModuleStr::Inner,
+                        typ_str: String::from("u32"),
                     }
-                }
+                };
             },
             _ => {
-                unimplemented!("{:?}", typ);
+                unimplemented!();
             }
         }
     }
 
-    fn gen_single_operate(self, node: Box<SingleOperator>) -> NodeData {
+    fn gen_single_operate(self, node: Box<SingleOperatorAstNode>) -> NodeData {
+        unimplemented!();
     }
 
-    fn gen_binary_operator(self, node: Box<BinaryOperator>) -> NodeData {
+    fn gen_binary_operator(self, node: Box<BinaryOperatorAstNode>) -> NodeData {
         /*
          * 生成指令
          * 1. 判断左边和右边的类型
@@ -67,6 +78,7 @@ impl Generator {
          *  - 从栈中读取几个参数
          *  - 返回值存放在哪里
          * */
+        unimplemented!();
     }
 
     pub fn new() -> Self {
