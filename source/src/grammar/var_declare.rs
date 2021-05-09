d43fbadd7d419b736ff534484007aa3b66164f2a
lions-language <lions_language@163.com>
2021-05-09 09:20:18 +0800

    add var declare

---
+++ source/src/grammar/var_declare.rs
@@ -0,0 +1,19 @@
+use crate::lexical;
+
+struct VarDeclare {
+}
+
+impl VarDeclare {
+    /*
+     * 语句: int a = 1
+     * */
+    pub fn parse1() {
+        let lexical = lexical::Lexical::new();
+    }
+
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+}
+
