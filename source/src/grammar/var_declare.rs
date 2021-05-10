c9bf4d341533647ef8c7c8e0281e5edc5c3d8abd
lions-language <lions_language@163.com>
2021-05-10 23:22:42 +0800

    var declare

--- source/src/grammar/var_declare.rs
+++ source/src/grammar/var_declare.rs
@@ -6,9 +6,20 @@ struct VarDeclare {
 impl VarDeclare {
     /*
      * 语句: int a = 1
+     * 文法: Intdeclare: 'int' Identify '=' Number
      * */
     pub fn parse1() {
-        let lexical = lexical::Lexical::new();
+        let content = r"
+        int a = 1
+        ";
+        let mut lexical = lexical::Lexical::new(content.as_bytes().to_vec());
+        match lexical.lookup_next_one() {
+            Some(n) => {
+            },
+            None => {
+                panic!("expect int");
+            }
+        }
     }
 
     pub fn new() -> Self {
