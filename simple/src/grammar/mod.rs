673864b4eb3410533c73af12be0ffa0455268492
lions-language <lions_language@163.com>
2021-05-21 23:13:08 +0800

    add grammar operater

--- simple/src/grammar/mod.rs
+++ simple/src/grammar/mod.rs
@@ -6,6 +6,37 @@ pub struct Grammar {
 }
 
 impl Grammar {
+    pub fn lookup_next_n(&self, n: usize) -> Option<&token::Token> {
+        if n > self.tokens.len() {
+            return None;
+        }
+        self.tokens.get(n - 1)
+    }
+
+    pub fn lookup_next_one(&self) -> Option<&token::Token> {
+        self.lookup_next_n(1)
+    }
+
+    pub fn take_next_one(&mut self) -> Option<token::Token> {
+        if self.tokens.len() < 1 {
+            return None;
+        }
+        self.tokens.pop_front()
+    }
+
+    pub fn skip_next_n(&mut self, n: usize) {
+        if n > self.tokens.len() {
+            return;
+        }
+        for _ in 0..n {
+            self.tokens.pop_front();
+        }
+    }
+
+    pub fn skip_next_one(&mut self) {
+        self.skip_next_n(1);
+    }
+
     pub fn new(tokens: VecDeque<token::Token>) -> Self {
         Self {
             tokens: VecDeque::new()
